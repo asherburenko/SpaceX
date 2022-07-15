e2db3550d13ebc1834c232601f38d9fe58a5ca7f
A.Sherburenko <asherburenko@mail.ru>
2022-07-15 15:23:36 +0500

    Парсинг данных
    заполнил все данные первого экрана, заполнил collectionView, реализовал экран настройки, теперь при изменении настроек, данные в коллекции меняются

---
+++ SpaceX/SpaceX/Model/CollectionViewData.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewData.swift
+//  SpaceX
+//
+//  Created by Александр Шербуренко on 15.07.2022.
+//
+
+import Foundation
+
+struct CollectionViewData {
+    var height: Double
+    var diametr: Double
+    var mass: Double
+    var payLoadpayloadWeights: Double
+    var heightDescription: String
+    var diametrDescription: String
+    var massDescription: String
+    var payLoadpayloadWeightsDescription: String
+}
